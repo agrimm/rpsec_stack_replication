1890c087c83c087f1cf8475f836fa057705b2461
Andrew Grimm <andrew.j.grimm@gmail.com>
2015-10-03 21:43:21 +1000

    Generate example of RSpec and decoration not working together

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 describe UserDecorator do
+  let(:user) { User.new }
+  let(:decorated_user) { user.decorate }
+
+  describe '#greeting' do
+    # Failing spec
+    it 'uses DecoratedUser#name' do
+      expect(decorated_user).to receive(:name).and_call_original
+      decorated_user.greeting
+    end
+
+    # Successful version
+    it 'uses User#name' do
+      expect(user).to receive(:name).and_call_original
+      decorated_user.greeting
+    end
+
+    # Also successful, because it's now somehow properly set up
+    it 'again uses DecoratedUser#name' do
+      expect(decorated_user).to receive(:name).and_call_original
+      decorated_user.greeting
+    end
+  end
 end
